192974e97eed2a94c2a96cb8d21456ca0585e16c
JohnByrneRepo <html5css3@outlook.com>
2013-08-07 12:45:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,18 @@
 <html>
     <head>
         <?php include 'header.php'; ?>
+        <?php include 'loginPanel.php'; ?> 
     </head>
-    <body>        
-       <?php include 'loginPanel.php'; ?>
-        
-        <br><br><br><br><br>
-        
+    <body>            
         <?php
        
+        /*
         mysql_connect("localhost", "root", "") or die(mysql_error());
 	mysql_select_db("maindatabase") or die(mysql_error());
 
 	$result = mysql_query("SELECT * FROM users") 
 	or die(mysql_error());  
 	
-	/* Debug database connection
 	while($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
             echo ' | ';
             foreach($line as $col_value) {
@@ -24,10 +21,11 @@
             } 
             echo '<br><p>';
 	}
-	*/
+
 	
         echo $result;
-        ?>
         
+        */
+        ?> 
     </body>
 </html>
