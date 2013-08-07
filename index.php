723de30d12f53076ed373e6120d82972f2554f73
JohnByrneRepo <html5css3@outlook.com>
2013-08-07 12:44:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,23 +9,23 @@
         <br><br><br><br><br>
         
         <?php
-        // Make a MySQL Connection
-	mysql_connect("localhost", "root", "") or die(mysql_error());
+       
+        mysql_connect("localhost", "root", "") or die(mysql_error());
 	mysql_select_db("maindatabase") or die(mysql_error());
 
-	// Get all the data from the "example" table
 	$result = mysql_query("SELECT * FROM users") 
 	or die(mysql_error());  
-		
+	
+	/* Debug database connection
 	while($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
             echo ' | ';
             foreach($line as $col_value) {
-                if ($col_value != 'Resource id #4') print "$col_value";
                 echo ' | ';
             } 
             echo '<br><p>';
 	}
-		
+	*/
+	
         echo $result;
         ?>
         
